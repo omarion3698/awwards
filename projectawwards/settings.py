21340486461fc72e5386c5ff05acd2ea7f26719e
omarion3698 <omaribinbakarivic@gmail.com>
2020-10-23 07:24:09 +0300

    Hide Secret Key

--- projectawwards/settings.py
+++ projectawwards/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0)fltz=&%%44*edht+$3f4h03j=doo3)0n&2wz912dj64^4a2k'
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
